3dfcdd8a0efc4b57a251a6bf4a176f136e6be193
EWEDLCM <168160898+EWEDLCM@users.noreply.github.com>
2025-08-19 18:01:18 +0800

    Update __init__.py

--- plugins/fnmvscheduler/__init__.py
+++ plugins/fnmvscheduler/__init__.py
@@ -32,7 +32,7 @@ class Fnmvscheduler(_PluginBase):
     # 插件图标
     plugin_icon = "https://raw.githubusercontent.com/EWEDLCM/MoviePilot-Plugins/main/icons/fnmv.png"
     # 插件版本
-    plugin_version = "1.2.1" 
+    plugin_version = "1.2.2" 
     # 插件作者
     plugin_author = "EWEDL"
     # 作者主页
@@ -526,4 +526,5 @@ class Fnmvscheduler(_PluginBase):
             from app.core.event import eventmanager
             eventmanager.remove_event_listener(EventType.TransferComplete, self.handle_transfer_complete)
         except Exception as e:
+
             logger.debug(f"【飞牛影视调度器】注销事件监听器时出错（可能已被注销）: {e}")
